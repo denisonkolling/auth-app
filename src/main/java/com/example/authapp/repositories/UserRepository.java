224efb7307c2802903f08ee7f42281c26dab01aa
Denison Kolling <denisonkolling@gmail.com>
2023-08-29 09:07:10 -0300

    add user model and role

--- src/main/java/com/example/authapp/repositories/UserRepository.java
+++ src/main/java/com/example/authapp/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.authapp.repositories;
 
-public class UserRepository {
+import com.example.authapp.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    UserDetails findByLogin(String login);
 }
