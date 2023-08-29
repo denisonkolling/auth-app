2b15335769ff9a03b5d58219d25591216d9026ae
Denison Kolling <denisonkolling@gmail.com>
2023-08-29 09:56:26 -0300

    add security configuration

--- src/main/java/com/example/authapp/infra/security/SecurityConfiguration.java
+++ src/main/java/com/example/authapp/infra/security/SecurityConfiguration.java
@@ -2,10 +2,13 @@ package com.example.authapp.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.authorization.AuthenticatedAuthorizationManager.authenticated;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +17,12 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(csrf -> csrf.disable()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .build();
     }
 }
